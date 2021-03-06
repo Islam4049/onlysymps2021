6ad86b272bb1ce068e2b969ce40ecf00e31b9670
Islam4049 <70667310+Islam4049@users.noreply.github.com>
2021-03-06 00:05:42 -0500

    change in server.js

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const mongoose = require ("mongoose");
 const routes= require ("./routes")
-
+const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -13,7 +14,6 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-
 
 // Define API routes here
 app.use(routes)
